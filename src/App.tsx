0cab1cf4903fd6bc2dd08c2a7b5eda5d7e2808de
Venchhinh <houtvinching@gmail.com>
2024-06-21 22:09:47 +0700

    Team Up

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ function App() {
   return (
     <>
      <h1>Hello Students Deploy Git in Vercel</h1>
-     <h1>Team Up with Students</h1>
-     <h2>About user</h2>
+     <h1>I Love Me</h1>
     </>
   )
 }
